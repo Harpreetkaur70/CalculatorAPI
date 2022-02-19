a09c5e386dc262cd088a363004d42a19808d62fb
Harpreetkaur70 <harpreetkaur11379@gmail.com>
2022-02-19 20:42:44 +0530

    ProjectSetup

---
+++ CalculatorAPI/CalculatorTests/CalculatorTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestAddition()
+        {
+
+        }
+    }
+}
