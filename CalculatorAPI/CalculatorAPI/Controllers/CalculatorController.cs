3212b1d39a94b850becb82699ea84717cb9e5c76
Harpreetkaur70 <harpreetkaur11379@gmail.com>
2022-02-19 20:52:16 +0530

    Api Setup

--- CalculatorAPI/CalculatorAPI/Controllers/CalculatorController.cs
+++ CalculatorAPI/CalculatorAPI/Controllers/CalculatorController.cs
@@ -8,6 +8,34 @@ namespace CalculatorAPI.Controllers
     [ApiController]
     public class CalculatorController : ControllerBase
     {
-        
+        private readonly IMath _math;
+        public CalculatorController(IMath math)
+        {
+            _math = math;
+        }
+
+        [HttpGet("Addition")]
+        public int Addition(int i, int j)
+        {
+            return _math.Addition(i, j);
+        }
+
+        [HttpGet("Substraction")]
+        public int Substraction(int i, int j)
+        {
+            return _math.Substraction(i, j);
+        }
+
+        [HttpGet("Multiplication")]
+        public int Multiplication(int i, int j)
+        {
+            return _math.Multiplication(i, j);
+        }
+
+        [HttpGet("Division")]
+        public int Divsion(int i, int j)
+        {
+            return _math.Divsion(i, j);
+        }
     }
 }
